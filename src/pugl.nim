a2d9f44cbd9cc4a735912586ff0559d5818f2603
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2024-08-16 02:45:45 -0400

    fix broken clang includes by generating absolute paths

--- src/pugl.nim
+++ src/pugl.nim
@@ -12,6 +12,7 @@
 #         # "gl.h"
 #         # "glu.h"
 import pugl/[pugl_header, gl]
+import std/[os] #, paths]
 export pugl_header, gl
 
 proc puglGetProcAddress*(name: cstring): pointer {.cdecl, importc: "puglGetProcAddress".}
@@ -19,15 +20,21 @@ proc puglEnterContext*(view: Puglview): Puglstatus {.cdecl, importc: "puglEnterC
 proc puglLeaveContext*(view: Puglview): Puglstatus {.cdecl, importc: "puglLeaveContext".}
 proc puglGlBackend*(): ptr Puglbackend {.cdecl, importc: "puglGlBackend".}
 
+# var package_path = Path(getAppDir())
+# echo("-v -I" & (currentSourcePath().parentDir() / "pugl" / "pugl_source" / "include"))
+# echo(currentSourcePath().parentDir())
+
 when defined(macosx):
     {.passl: "-framework OpenGL".}
     {.passl: "-framework Foundation".}
     {.passl: "-framework CoreGraphics".}
     {.passl: "-framework CoreVideo".}
     {.passl: "-framework Cocoa".}
-    {.compile("pugl/includes_mac.m", "-Isrc/pugl/pugl_source/include") .}
+    # kinda a kludge and poisons syntax highlighting but it fixes an issue with clang not finding the relative include path
+    {.compile(currentSourcePath().parentDir() / "pugl" / "includes_mac.m", "-I" & (currentSourcePath().parentDir() / "pugl" / "pugl_source" / "include")) .}
 else:
-    {.compile("pugl/includes.c", "-v -Isrc/pugl/pugl_source/include") .}
+    #TODO make sure the paths are interpreted correctly on windows
+    {.compile(currentSourcePath().parentDir() / "pugl" / "includes.c", "-I" & (currentSourcePath().parentDir() / "pugl" / "pugl_source" / "include")) .}
 
 # var foo = puglGlBackend()
 # echo(foo)
